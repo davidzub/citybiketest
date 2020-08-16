3bd1439991405842f81b6be1a6be2459759ae6ac
David Zuñiga <davidzub@gmail.com>
2020-08-15 21:32:36 -0500

    Test

--- citybike-server/server.js
+++ citybike-server/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const http = require("http");
 const socketIo = require("socket.io");
+const axios = require("axios");
 const citybikeurl = "http://api.citybik.es/v2/networks/decobike-miami-beach"
 
 
@@ -19,15 +20,24 @@ let interval;
 io.on("connection", socket => {
   var socketId = socket.id;
   var clientIp = socket.request.connection.remoteAddress;
+  if (interval) {
+    clearInterval(interval);
+  }
+  interval = setInterval(() => getApiAndEmit(socket), 1000);
   console.log('New connection ' + socketId + ' from ' + clientIp);
   socket.on("disconnect", () => {
     console.log("Client disconnected");
+    clearInterval(interval);
   });
+  
 });
-
-
-
+const getApiAndEmit = socket => {
+    axios.get(
+      citybikeurl
+    ).then(res => {
+      const data = res.data.network
+      socket.emit("FromAPI", data);
+    })
+  
+};
 server.listen(port, () => console.log(`Listening on port ${port}`));
-
-
-
